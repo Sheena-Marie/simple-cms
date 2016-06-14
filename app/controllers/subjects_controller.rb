7b6c0bf5be5a33dc5ec9a8a0bb03b5f8a817414f
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-06-14 20:42:32 +1000

    added required stuff for subjects controller and views

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -20,6 +20,7 @@ class SubjectsController < ApplicationController
     # Save the object
     if @subject.save
     # If save succeeds, redirect to the index action
+      flash[:notice] = "Subject created successfully"
       redirect_to(:action => 'index')
     else
     # If save fails, redisplay the form so user can fix problems
@@ -28,9 +29,31 @@ class SubjectsController < ApplicationController
   end
 
   def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    # Find an existing object using form parameters
+    @subject = Subject.find(params[:id])
+    # update the object
+    if @subject.update_attributes(subject_params)
+    # If update succeeds, redirect to the index action
+      flash[:notice] = "Subject updated successfully"
+      redirect_to(:action => 'show', :id => @subject.id)
+    else
+    # If update fails, redisplay the form so user can fix problems
+      render('edit')
+    end
   end
 
   def delete
+    @subject = Subject.find(params[:id])
+  end
+
+  def destroy
+    subject = Subject.find(params[:id]).destroy
+    flash[:notice] = "Subject '#{subject.name}' destroyed successfully"
+    redirect_to(:action => 'index')
   end
 
   private
